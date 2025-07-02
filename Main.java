31d6c612d43f9ff8f750460e8ee13afb53025e4b
VinayakJhingan <127829351+VinayakJhingan@users.noreply.github.com>
2025-07-02 11:45:25 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float floatValue1 = 2.0f;
+        float floatValue2 = 4.0f;
+        float floatResult = floatValue1+floatValue2;
+
+        System.out.println("Float: "+floatResult);
+
+
+        double doubleValue1 = 2.0;
+        double doubleValue2 = 4.0;
+        double doubleResult = doubleValue1 + doubleValue2;
+
+        System.out.println("Double: "+doubleResult);
+
+
+
+
+
+    }
+
+
+
+
+
+}
