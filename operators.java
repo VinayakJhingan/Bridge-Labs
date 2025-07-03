78223e39b06cd0deff2ebafc542a25429cdfe3a0
VinayakJhingan <127829351+VinayakJhingan@users.noreply.github.com>
2025-07-03 12:23:22 +0530

    Add files via upload

---
+++ operators.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class operators {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 5;
+        int c = a+b;
+        int d = a-b;
+        int e = a*b;
+        int f = a/b;
+        int g = a%b;
+        int h = a++;
+
+        System.out.println("Addition of "+ a +" and "+ b +" is "+c);
+        System.out.println("Subtraction of "+a+" and "+b+" is "+d);
+        System.out.println("Subtraction of "+a+" and "+b+" is "+d);
+        System.out.println("Multiplication of "+a+" and "+b+" is "+e);
+        System.out.println("Division of "+a+" and "+b+" is "+f);
+        System.out.println("Modulus of "+a+" and "+b+"is "+g);
+        System.out.println("increment of "+a+" and "+b+" is "+h);
+
+    }
+}
