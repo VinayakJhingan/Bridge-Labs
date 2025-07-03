1e2427ccca829c5948b8af28ea80c97a984ec52a
VinayakJhingan <127829351+VinayakJhingan@users.noreply.github.com>
2025-07-03 12:19:52 +0530

    Add files via upload

--- First.java
+++ First.java
@@ -1,7 +1,19 @@
+import java.util.Scanner;
+
 public class First {
     public static void main (String[] args) {
-        System.out.println("Hello world");
-        System.out.println("Ram_Rahim");
+       // System.out.println("Hello World");
+        // System.out.println("Ram_Rahim");
+//        Scanner sc = new Scanner(System.in);
+//        String str = sc.nextLine();
+//        System.out.println("Hello" + str);
+
+
+
+        System.out.println("Hello");
+        for(int i = 0; i <= args.length - 1; i++){
+            System.out.println(args[i]);
+        }
     }
 
 }
